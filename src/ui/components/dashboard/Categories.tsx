b587c17d223d8b926b4af00b70d9f7299f963ffb
Francis <francisbernal14@gmail.com>
2024-01-10 21:32:45 +0100

    feat: create categories component dashboard and fix somethings

--- src/ui/components/dashboard/Categories.tsx
+++ src/ui/components/dashboard/Categories.tsx
@@ -3,16 +3,23 @@ import { Category } from "@prisma/client";
 export async function Categories({ categories }: { categories: Category[] }) {
   return (
     <div>
+      <span>Selecciona una categoria:</span>
       {categories.map((category) => {
         return (
           <div key={category.id} className="ct-flex-row">
             <input
-              type="checkbox"
+              type="radio"
               name="category"
               value={category.id}
               id={category.name}
+              className="peer hidden"
             />
-            <label htmlFor={category.name}>{category.name}</label>
+            <label
+              htmlFor={category.name}
+              className="peer-checked:border-green-400 btn-link px-2 py-1"
+            >
+              {category.name}
+            </label>
           </div>
         );
       })}
