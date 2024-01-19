af34d79d9613123ccad697e4282c6d4578c28c09
Francis <francisbernal14@gmail.com>
2024-01-19 20:19:50 +0100

    fix nav link

--- src/ui/components/Nav.tsx
+++ src/ui/components/Nav.tsx
@@ -6,7 +6,7 @@ export default function Nav({ customClass, handleModal }: { customClass: string 
     return (
         <nav className={`max-md:ct-flex-col max-md:items-start max-md:text-2xl ct-flex-row gap-6 justify-center text-opposite ${customClass}`}>
             <Link onClick={handleModal} href="/" className="hover:scale-110 transition-transform">Home</Link>
-            <Link onClick={handleModal} href="" className="hover:scale-110 transition-transform">About me</Link>
+            <Link onClick={handleModal} href="/about-me" className="hover:scale-110 transition-transform">About me</Link>
             <Link onClick={handleModal} href="/projects" className="hover:scale-110 transition-transform">Projects</Link>
             <Link onClick={handleModal} href="/aprende" className="hover:scale-110 transition-transform">Learn</Link>
         </nav>
