25a3f9368b3c342df72e2508eb0982ae4adbe7eb
Francis <francisbernal14@gmail.com>
2024-02-01 21:41:34 +0100

    add metadata in dynamic category route

--- src/app/[lang]/aprende/[category]/page.tsx
+++ src/app/[lang]/aprende/[category]/page.tsx
@@ -2,6 +2,28 @@ import getDictionary from "@/dictionaries/dictionaries";
 import { fetchAllCategories } from "@/lib/dbdata";
 import { Lang } from "@/lib/definitions";
 import LatestPosts from "@/ui/components/aprende/LatestPosts";
+import { Metadata, ResolvingMetadata } from "next";
+
+export async function generateMetadata(
+  { params: { lang, category } }: { params: { lang: Lang; category: string } },
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const fullCategory = (await fetchAllCategories()).find(
+    (item) => item.name.toLowerCase() === category
+  );
+  const title = lang === "es" ? fullCategory?.title_es : fullCategory?.title_en;
+  const description =
+    lang === "es" ? fullCategory?.description_es : fullCategory?.description_en;
+  return {
+    title: title,
+    description: description,
+    keywords: ["Programación", "web", "curso", `${category}`, "Málaga"],
+    referrer: "strict-origin-when-cross-origin",
+    openGraph: {
+      images: "",
+    },
+  };
+}
 
 export const dynamicParams = false;
 
