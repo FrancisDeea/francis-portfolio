373d6fee74783ea72d5a860827574d93ee688f81
Francis <francisbernal14@gmail.com>
2024-02-04 22:50:20 +0100

    fix title component

--- src/ui/components/dashboard/DashPostCard.tsx
+++ src/ui/components/dashboard/DashPostCard.tsx
@@ -6,13 +6,13 @@ import { DeleteIcon, ViewIcon, EditIcon, findIcon } from "@/ui/icons";
 import { Post } from "@prisma/client";
 
 export default function DashPostCard({ post }: { post: Post }) {
-  const { id, title, hashtags } = post;
+  const { id, title_es, hashtags } = post;
 
   return (
     <div className="ct-flex-row items-stretch border border-medium rounded-md p-4 bg-background2 max-lg:gap-6 max-lg:overflow-scroll">
       <div className="ct-flex-col justify-between gap-1 flex-1 max-lg:min-w-max">
         <span className="">Título</span>
-        <span className="font-semibold">{title}</span>
+        <span className="font-semibold">{title_es}</span>
       </div>
       <div className="ct-flex-col justify-between gap-1 flex-1 max-lg:min-w-max">
         <span className="">Hashtags</span>
